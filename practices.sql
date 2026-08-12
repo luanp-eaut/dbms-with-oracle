dfc9ffaba77e768efae5eb08364699d35b19cdb5
nplua <nguyenphonlua@gmail.com>
2026-08-12 11:15:08 +0700

    update code

--- practices.sql
+++ practices.sql
@@ -1,19 +1,3 @@
    set transaction read only;
-insert into emp (
-   empno,
-   ename,
-   job,
-   mgr,
-   hiredate,
-   sal,
-   comm,
-   deptno
-) values
-   ( 80011,
-     'LE VAN A',
-     'MANAGER',
-     7839,
-     sysdate,
-     5000,
-     null,
-     60 );
+select *
+  from emp;
